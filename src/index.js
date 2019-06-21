7e6c3aa4ff707b9d2a5707e061f2e86598df7812
Yoidea <yoidea.cc@gmail.com>
2019-06-22 02:20:45 +0900

    :hammer: Refactor code

--- src/index.js
+++ src/index.js
@@ -1,57 +1,59 @@
 import $ from "jquery";
 
-function getChats() {
-  const results = $.ajax({
-    url: "https://gachat-api.herokuapp.com/graphql",
-    contentType: "application/json",
-    type: "POST",
-    async: false,
-    data: JSON.stringify({
-      query: `{allChats {name body}}`
-    })
-  }).responseJSON.data.allChats;
-  return results;
-}
+class Gachat {
+  constructor() {
+    this.baseurl = "https://gachat-api.herokuapp.com/graphql";
+    this.render();
+  }
 
-function appendChat(name, body) {
-  const chat = $("<div>");
-  const dispName = $("<p>").append($("<strong>").text(`${name} さん`));
-  chat.append(dispName);
-  chat.append($("<p>").text(body));
-  chat.append($("<hr>"));
-  $("#chats").append(chat);
-}
+  getChats() {
+    const results = $.ajax({
+      url: this.baseurl,
+      contentType: "application/json",
+      type: "POST",
+      async: false,
+      data: JSON.stringify({
+        query: `{allChats {name body}}`
+      })
+    }).responseJSON.data.allChats;
+    return results;
+  }
 
-function postChat() {
-  const name = $("#name").val();
-  const body = $("#body").val();
-  $("#body").val("");
-  $.ajax({
-    url: "https://gachat-api.herokuapp.com/graphql",
-    contentType: "application/json",
-    type: "POST",
-    async: false,
-    data: JSON.stringify({
-      query: `mutation {createChat(body: "${body}", name: "${name}") {id} }`
-    })
-  }).responseJSON.data.id;
-  refresh();
-}
+  postChat(name, body) {
+    $.ajax({
+      url: this.baseurl,
+      contentType: "application/json",
+      type: "POST",
+      async: false,
+      data: JSON.stringify({
+        query: `mutation {createChat(name: "${name}", body: "${body}") {id} }`
+      })
+    }).responseJSON.data.id;
+  }
 
-function refresh() {
-  const chats = getChats();
-  $("#chats").empty();
-  chats.forEach(c => {
-    appendChat(c.name, c.body);
-  });
+  render() {
+    const area = $("#chats");
+    area.append($("<p>").text("読み込み中..."));
+    const chats = this.getChats();
+    area.empty();
+    chats.forEach(c => {
+      const chat = $("<div>");
+      const name = $("<p>").append($("<strong>").text(`${c.name} さん`));
+      chat.append(name);
+      chat.append($("<p>").text(c.body));
+      chat.append($("<hr>"));
+      area.append(chat);
+    });
+  }
 }
 
 $(() => {
-  $("#chats").text("読み込み中...");
-  const chats = getChats();
-  $("#chats").empty();
-  chats.forEach(c => {
-    appendChat(c.name, c.body);
+  const gachat = new Gachat();
+  $("#post").click(() => {
+    const name = $("#name").val();
+    const body = $("#body").val();
+    $("#body").val("");
+    gachat.postChat(name, body);
+    gachat.render();
   });
-  $("#post").click(postChat);
 });

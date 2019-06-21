cd3126667d6e9735b901607f6e066f2599fc10cb
Yoidea <yoidea.cc@gmail.com>
2019-06-22 03:00:13 +0900

    :hammer: Use fetch

--- src/index.js
+++ src/index.js
@@ -6,35 +6,45 @@ class Gachat {
     this.render();
   }
 
-  getChats() {
-    const results = $.ajax({
-      url: this.baseurl,
-      contentType: "application/json",
-      type: "POST",
-      async: false,
-      data: JSON.stringify({
-        query: `{allChats {name body}}`
-      })
-    }).responseJSON.data.allChats;
-    return results;
+  async getChats() {
+    const results = await fetch(this.baseurl, {
+      method: "POST",
+      body: JSON.stringify({
+        query: `query {
+          allChats {
+            name
+            body
+          }
+        }`
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => response.json());
+    return results.data.allChats;
   }
 
-  postChat(name, body) {
-    $.ajax({
-      url: this.baseurl,
-      contentType: "application/json",
-      type: "POST",
-      async: false,
-      data: JSON.stringify({
-        query: `mutation {createChat(name: "${name}", body: "${body}") {id} }`
-      })
-    }).responseJSON.data.id;
+  async postChat(name, body) {
+    const result = await fetch(this.baseurl, {
+      method: "POST",
+      body: JSON.stringify({
+        query: `mutation {
+          createChat(name: "${name}", body: "${body}") {
+            id
+          }
+        }`
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(response => response.json());
+    return result;
   }
 
-  render() {
+  async render() {
     const area = $("#chats");
     area.append($("<p>").text("読み込み中..."));
-    const chats = this.getChats();
+    const chats = await this.getChats();
     area.empty();
     chats.forEach(c => {
       const chat = $("<div>");
@@ -53,7 +63,8 @@ $(() => {
     const name = $("#name").val();
     const body = $("#body").val();
     $("#body").val("");
-    gachat.postChat(name, body);
-    gachat.render();
+    gachat.postChat(name, body).then(() => {
+      gachat.render();
+    });
   });
 });
